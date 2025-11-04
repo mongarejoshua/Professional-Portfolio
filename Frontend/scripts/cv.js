73895912aea54d8b53fa049df63c52fbf5ff797e
mongarejoshua <mogakahjoshua254@gmail.com>
2025-11-04 08:02:55 +0300

    4th commit

--- Frontend/scripts/cv.js
+++
@@ -1,9 +0,0 @@
-const cvButton = document.getElementById('button')
-
-cvButton.addEventListener('click', function () {
-
-    const fileLocation = "../files/cv.docx"
-    const fileName = "cv.docx"
-
-    
-})
