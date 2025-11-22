36c7d55992b0829593e5bfa711833b37491e7222
mongarejoshua <mogakahjoshua254@gmail.com>
2025-11-23 00:18:26 +0300

    update

--- client/assets/scripts/hero.js
+++ client/assets/scripts/hero.js
@@ -4,20 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!downloadBtn) return;
 
     downloadBtn.addEventListener("click", async () => {
-    const response = await fetch("./public/files/cv.pdf");
-    const blob = await response.blob();
-    const url = URL.createObjectURL(blob);
-    
-    setTimeout(() => {
-        const userWantsToOpen = confirm("File downloaded successfully!\nDo you want to open it?");
-        if (userWantsToOpen) {
-            const link = document.createElement("a")
-            link.href = "./public/files/cv.pdf"
-            link.download = "curriculum_vitae"
-            link.click()
-        }
-        URL.revokeObjectURL(url); // clean up
-    }, 1000); 
+        const response = await fetch("./public/files/cv.pdf");
+        const blob = await response.blob();
+        const url = URL.createObjectURL(blob);
+
+        setTimeout(() => {
+            const userWantsToOpen = confirm("File downloaded successfully!\nDo you want to open it?");
+            if (userWantsToOpen) {
+                const link = document.createElement("a")
+                link.href = "./public/files/cv.pdf"
+                link.download = "curriculum_vitae"
+                link.click()
+            }
+            URL.revokeObjectURL(url); // clean up
+        }, 1000);
     });
 
     window.addEventListener('scroll', function () {
